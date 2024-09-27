74325487bbf19f47b60ede15e06d6172b9c5e123
Derek <MotoDTop1@gmail.com>
2024-09-26 18:33:16 -0600

    added list topic functions

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topics.length; x++) {
- console.log(topics[x]);
-}
-
-
-/*
+function listTopics() {
+  for (let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+ }
+ 
+function selectTopic() {
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -17,4 +19,10 @@ if (topic === 'HTML') {
 } else {
  console.log('Please try again!');
 }
-*/
+}
+
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
