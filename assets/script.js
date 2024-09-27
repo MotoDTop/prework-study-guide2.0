80725c0b45174f49edb95e9fc8a32f86f4c47acb
Derek <MotoDTop1@gmail.com>
2024-09-26 18:46:06 -0600

    added random topic feature to js

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,13 @@ function listTopics() {
  }
  
 function selectTopic() {
-if (topic === 'HTML') {
+if (randomTopic === 'HTML') {
  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
